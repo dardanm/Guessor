02bc62ee25eaaaaaa930945835ed0e9a8ceeb458
Dardan Meha <mdardan20@gmail.com>
2017-06-28 01:30:55 -0400

    Added stopTimer() after app goes to foreground

--- Guessor/AppDelegate.swift
+++ Guessor/AppDelegate.swift
@@ -24,6 +24,7 @@ class AppDelegate: BaseLevel, UIApplicationDelegate {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
         print("applicationWillResignActive")
+        stopTimer()
     }
     
     func applicationDidEnterBackground(_ application: UIApplication) {
@@ -31,6 +32,7 @@ class AppDelegate: BaseLevel, UIApplicationDelegate {
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
         coin = UserDefaults.standard.integer(forKey: "MoneyKey")
         print("applicationDidEnterBackground")
+        stopTimer()
     }
     
     func applicationWillEnterForeground(_ application: UIApplication) {
